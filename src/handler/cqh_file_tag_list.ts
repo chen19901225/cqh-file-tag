d255b8885c93b6b5eeb41fe6cb4e26da870fedab
test <test@qq.com>
2020-12-09 15:49:24 +0800

    test

--- src/handler/cqh_file_tag_list.ts
+++ src/handler/cqh_file_tag_list.ts
@@ -15,7 +15,7 @@ export async function cqh_file_tag_list(textEditor: vscode.TextEditor, edit: vsc
     // let snippet = null;
     let ext = path.extname(document.uri.fsPath);
     let snippet = getSnippetForExt(ext);
-    
+
     // for (let i = 0; i < config.byExt.length; i++) {
     //     let current_snippet = config.byExt[i];
     //     if (current_snippet.name === ext) {
@@ -37,24 +37,31 @@ export async function cqh_file_tag_list(textEditor: vscode.TextEditor, edit: vsc
     for (let line_index = 0; line_index < lines.length; line_index++) {
         let currentLine = lines[line_index];
         currentLine = currentLine.trim();
-        let start_text = snippet.list[0] + static_var.start_text;
-        if (currentLine.startsWith(start_text)) {
-            if (snippet.list[1] && !currentLine.endsWith(snippet.list[1])) {
-                continue;
-            }
-            let title = currentLine.slice(start_text.length);
-            let tag_piece_list = title.trim().split("||");
-            for(let tag of tag_piece_list) {
-                tag_list.push(tag);
-            }
-            
-            
-            // requestRanges.push([line_index, line_index + 1, title]);
-        }
+        let start_text_list = ['cqh_file_tag', 'cqh-file-tag']
+        for(let  start_text_piece of start_text_list) {
+
+            let start_text = snippet.list[0] + start_text_piece;
+            if (currentLine.startsWith(start_text)) {
+                if (snippet.list[1] && !currentLine.endsWith(snippet.list[1])) {
+                    continue;
+                }
+                let title = currentLine.slice(start_text.length);
+                if(snippet.list[1]) {
+                    title = title.slice(title.length-snippet.list[1].length);
+                }
+                let tag_piece_list = title.trim().split("||");
+                for(let tag of tag_piece_list) {
+                    tag_list.push(tag);
+                }
+                // requestRanges.push([line_index, line_index + 1, title]);
+            }// // if currentLine.startsWith
+
+        } // for
+
 
     }
 
-    
+
     if (tag_list.length == 0) {
         error_message = "列表为空"
         vscode.window.showErrorMessage(error_message);
